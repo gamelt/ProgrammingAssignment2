deb6fded67cffbf10347a4951f061497b2b0cece
gamelt <gameltravis@comcast.net>
2015-10-24 16:53:07 -0500

    Created function body
    Inserted my completed program's function body into the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix produces a function object that caches a matrix inverse for fast retrieval
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set = set,
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a makeCacheMatrix object, checks whether the matrix is cached, caches it if cached, and then returns the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x[[4]]() ## runs the getInv function to retrieve the inverse if present
+  if(!is.null(m)){
+    message("Getting cached data...")
+    return(m)
+  }
+  
+  data <- x[[2]]() ## runs the get function
+  m <- solve(data, ...)
+  x[[3]](m) ## runs the setInv function
+  return(m)
 }
